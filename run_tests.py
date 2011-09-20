6d86c1f9ad99842dc9259bbc84ac63f82554c3f6
Adrien Chauve <adrien.chauve@gmail.com>
2011-09-20 21:08:34 +0200

    Move test to tests dir

--- run_tests.py
+++ run_tests.py
@@ -1,15 +1,19 @@
 # -*- coding: utf-8 -*-
 
 import unittest
+import os.path as osp
 from metapy import parse_file
+
+TEST_DIR = 'tests'
+
+TEST_FILES = ('test_make_vector.cpp',
+    )
 
 class TestMetaPy(unittest.TestCase):
 
-    def test_parse_file(self):
-        test_files = ('test_make_vector.cpp',
-                )
-
-        for test_file in test_files:
+    def test(self):
+        for test_file in TEST_FILES:
+            test_file = osp.join(TEST_DIR, test_file)
             metapy_output = parse_file(test_file + '.meta')
             ref_output = open(test_file + '.ref').read()
             self.assertEquals(''.join(metapy_output), ref_output)
